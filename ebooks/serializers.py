646bb193d059533c3414eeb61dd6d65bde61f150
AhmedDR200 <alshwwhy212@gmail.com>
2023-11-25 13:32:33 +0200

    EbooksAPI Project Setup

---
+++ ebooks/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Ebook, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class EbookSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(read_only=True, many=True)
+    class Meta:
+        model = Ebook
+        fields = '__all__'
